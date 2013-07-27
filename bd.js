5f3565c82b48a419ac17b88f71a1c893878c5e7f
Everyday Creative Team <hey+github@everydaycreative.ca>
2013-07-26 22:06:55 -0400

    Update bd.js

--- bd.js
+++ bd.js
@@ -1,4 +1,10 @@
-//broser detect
+/*!
+ *  browser-detect  version: 0.1
+ *  Browser vendor, touch and retina conditional loading.
+ *  http://github.com/everydaycreative/browser-detect
+ *  Authors: @everydaycreative
+ *  Copyright 2013 Everyday Creative. LGPL licensed.
+ */
 (function($jquery, win){
   var $ = ($jquery === undefined)?null:$jquery;
   
